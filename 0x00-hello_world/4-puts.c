b2605efb4aeeddcce3a35285a9e43695fd051997
PhilipObiri <philipobiri3.1@gmail.com>
2023-02-16 07:45:08 -0800

    Updating the C program. To print exactly "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  * main -is the entry point for the program.
  *
- * Return:zero if no error, Non-zero value if errors 
+ * Return: zero is no error, Non-zero value if errors
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like  building a multilingual puzzle");
 	return (0);
 }
