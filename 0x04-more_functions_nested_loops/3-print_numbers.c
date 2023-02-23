9d04e8674236caaf344bdaccb6ddf07f2859615c
PhilipObiri <philipobiri3.1@gmail.com>
2023-02-23 10:13:28 -0800

    A function that prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,47 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Finds and prints the largest prime
- *        factor of the number 612852475143.
- *
- * Return: Always 0.
+ * print_numbers - Prints the numbers from 0-9.
  */
-int main(void)
+void print_numbers(void)
 {
-	long pf = 612852475143, div;
+	int num;
 
-	while (div < (pf / 2))
-	{
-		if ((pf % 2) == 0)
-		{
-			pf /= 2;
-			continue;
-		}
-
-		for (div = 3; div < (pf / 2); div += 2)
-		{
-			if ((pf % div) == 0)
-				pf /= div;
-		}
-	}
-
-	printf("%ld\n", pf);
-
-	return (0);
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+	_putchar('\n');
 }
-/* or
- * int main(void)
- * {
- * 	long int x = 612852475143;
- * 	long int p;
- *
- * 	for (p = 2; p < x; p++)
- * 	{
- * 		if (x % p == 0)
- * 		{
- * 			x = x / p;
- * 		}
- * 	}
- * 	printf("%ld\n", p);
- * 	return (0);
- * }
+
