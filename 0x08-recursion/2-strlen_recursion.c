551a70b65813e8e913da5710bf97d8803fdca37a
PhilipObiri <philipobiri3.1@gmail.com>
2023-03-08 09:29:57 -0800

    Write a function that returns the length of a string.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,47 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * check_strlen - return length of string
- * @s: string to check
- *
- * Return: length of s
+ * _strlen_recursion - return length of a string
+ * @s: string
+ * Return: length of string
  */
-int check_strlen(char *s)
+
+int _strlen_recursion(char *s)
 {
+	int c;
+
+	c = 0;
 	if (*s == '\0')
 		return (0);
-	else
-		return (1 + check_strlen(s + 1));
-}
 
-/**
- * check_palindrome - checks to see if a string is a palindrome
- * @str: string to check
- * @len: length of string
- * @p: possible palindrome
- *
- * Return: 1 if palindrome ,else 0
- */
-int check_palindrome(int str, int len, char *p)
-{
-	if (str >= len)
-		return (1);
-	else if (p[str] != p[len])
-		return (0);
-	else
-		return (check_palindrome(str + 1, len - 1, p));
-}
+	c = 1 +  _strlen_recursion(s + 1);
 
-/**
- * is_palindrome - states if a string is a palindrome
- * @s: string to check
- *
- * Return: 1 if palindrome, else 0
- */
-int is_palindrome(char *s)
-{
-	int i;
-
-	i = check_strlen(s) - 1;
-	return (check_palindrome(0, i, s));
+	return (c);
 }
